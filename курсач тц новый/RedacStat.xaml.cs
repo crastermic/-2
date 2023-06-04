951ef17036debafa2f1f22b58fd30657766fa9bf
craster <craster@LENOVO330>
2023-06-04 14:08:22 +1000

    ну работает ну нет

---
+++ курсач тц новый/RedacStat.xaml.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace курсач_тц_новый
+{
+    /// <summary>
+    /// Логика взаимодействия для RedacStat.xaml
+    /// </summary>
+    public partial class RedacStat : Window
+    {
+        public List<TblStat> TblStats { get; set; }
+        public bool Editable { get; set; }
+        public List<TblStat> tblStats = new List<TblStat>();
+        public List<TblStat> Stats = new List<TblStat>();
+
+        public TblStat SelectedDate { get; set; }
+        public TblStat SelectedStat { get; set; }
+        public TblStat SelectedStatistic { get; set; }
+        public List<TblCardhall> tblCardhalls { get; set; }
+
+        public RedacStat(TblStat selectedStatistic)
+        {
+            InitializeComponent();
+            DB.GetInstance().SaveChanges();
+            tblStats = DB.GetInstance().TblStats.ToList();
+            tblCardhalls = DB.GetInstance().TblCardhalls.ToList();
+
+            if (selectedStatistic.IdStat == 0 || selectedStatistic.IdStat == null)
+            {
+                SelectedStatistic = selectedStatistic;
+                SelectedStatistic = new TblStat();
+            }
+            else
+            {
+                SelectedStatistic = selectedStatistic.Clones();
+                Editable = true;
+
+            }
+
+            DataContext = this;
+        }
+
+        void Signal(string prop) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        public void Search()
+        {
+            Signal(nameof(Stats));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void SaveCloss(object sender, RoutedEventArgs e)
+        {
+
+            if (!Editable)
+            {
+                DB.GetInstance().TblStats.Add(SelectedStatistic);
+            }
+            else
+            {
+                var original = DB.GetInstance().TblStats.FirstOrDefault(x => x.IdStat == SelectedStatistic.IdStat);
+
+                // var original = DB.GetInstance().TblStats.
+                //Find(SelectedStatistic.IdStat);
+                // DB.GetInstance().TblStats.Entry(original)
+                //     .CurrentValues.SetValues(SelectedStatistic);
+
+
+            }
+            DB.GetInstance().SaveChanges();
+            Search();
+            Close();
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+    public static class ProductExtensions
+    {
+        public static TblStat Clones(this TblStat tblStat)
+        {
+            var values = DB.GetInstance().TblStats.Entry(tblStat).CurrentValues.Clone();
+            var clones = (TblStat)values.ToObject();
+            return clones;
+        }
+    }
+}
--- курсач тц новый/Stada.xaml.cs
+++ курсач тц новый/Stada.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,63 +25,91 @@ namespace курсач_тц_новый
     public partial class Stada : Window
     {
         public List<TblStat> TblStats { get; set; }
+        public List<TblStat> Stats = new List<TblStat>();
         public List<Stada> stadas;
         private TblStat SelectedStat { get; set; }
         public TblStat stat { get; set; }
         private TblStat Statred = new TblStat();
 
 
+        public TblStat SelectedStatistic { get => selectedStatistic;  set { selectedStatistic = value; Fill(); } }
+
+
         public List<TblPavilion> tblPavilion;
+        public List<TblStat> tblStat;
+        private TblStat selectedStatistic;
+
         public TblPavilion SelectedPavilion { get; }
         public Stada(TblStat selectedStat, TblPavilion selectedPavilion)
         {
             InitializeComponent();
-            Jop.ItemsSource = DB.GetInstance().TblStats.ToList();
+            //Jop.ItemsSource = DB.GetInstance().TblStats.ToList();
             TblStats = DB.GetInstance().TblStats.ToList();
-            //DataContext = new TblStat();
+
+
+            DataContext = new TblStat();
+
+
             DataContext = Statred;
             DataContext = SelectedStat;
 
             tblPavilion = DB.GetInstance().TblPavilions.ToList();
             SelectedPavilion = selectedPavilion;
 
+            tblStat = DB.GetInstance().TblStats.ToList();
             //SelectedStat = selectedStat;
-            //stat = new TblStat();
-            //stat = selectedStat;
+
+            SelectedStat = selectedStat;
+            stat = new TblStat();
+            stat = selectedStat;
+
             DataContext = this;
         }
 
         private void Sadd(object sender, RoutedEventArgs e)
         {
-            
-            
-            new TblStat();
-            TblStats = DB.GetInstance().TblStats.ToList();
-            SelectedStat = new TblStat();//мб понадобится сделать SelectedStat.и_тут_типо_навигэйшенс
-            DB.GetInstance().TblStats.Add(SelectedStat);
-            DB.GetInstance().SaveChanges();
+            new RedacStat(new TblStat()).ShowDialog();
             Signal(nameof(TblStats));
+
+            //new TblStat();
+            //TblStats = DB.GetInstance().TblStats.ToList();
+            //SelectedStat = new TblStat();//мб понадобится сделать SelectedStat.и_тут_типо_навигэйшенс
+            //DB.GetInstance().TblStats.Add(SelectedStat);
+            //DB.GetInstance().SaveChanges();
+            //Signal(nameof(TblStats));
         }
 
+        private void Fill([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
         private void Srem(object sender, RoutedEventArgs e)
         {
-
+            new RedacStat(SelectedStatistic).ShowDialog();
+            Search();
         }
+    
         private void Sdel(object sender, RoutedEventArgs e)
         {
-            var result = MessageBox.Show("Вы действительно хотите удалить эту запись?", "Подтвердить", MessageBoxButton.YesNo);
-            if (result == MessageBoxResult.Yes)
+            var result2 = MessageBox.Show("Вы действительно хотите удалить эту запись?", "Подтвердить", MessageBoxButton.YesNo);
+            if (result2 == MessageBoxResult.Yes)
             {
-                var db = DB.GetInstance();
-                db.TblStats.Remove(SelectedStat);
-                SelectedStat = null;
-                db.SaveChanges();
-                Signal(nameof(TblStats));
+
+                var remove = DB.GetInstance().TblStats.Find(SelectedStatistic.IdStat);
+                DB.GetInstance().TblStats.Remove(remove);
+               
+                tblStat = DB.GetInstance().TblStats.Where(s => s.IdStat == SelectedStatistic.IdStat).ToList();
+                DB.GetInstance().SaveChanges();
+                Search();
+                InitializeComponent();
             }
         }
         public event PropertyChangedEventHandler? PropertyChanged;
         void Signal(string prop) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        public void Search()
+        {
+            Signal(nameof(Stats));
+        }
 
-       
     }
 }
